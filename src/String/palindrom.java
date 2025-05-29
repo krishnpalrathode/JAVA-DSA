9c8cf078d421ffc4d059001b70edcc2000edaaeb
krishnpalrathode <rajputkrishna9399@gmail.com>
2025-05-29 08:52:02 +0530

    initial DSA commit

--- src/String/palindrom.java
+++ src/String/palindrom.java
@@ -2,21 +2,16 @@ package String;
 
 public class palindrom {
 
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
     public static void main(String[] args){
-        String str1 = "MADAN";
-        String str2 ="";
-
-        for(int i = str1.length()-1;i>=0;i--){
-            str2 = str2 + str1.charAt(i);
-
-        }
-        System.out.println(str2);
-        if(str1.equals(str2)){
-            System.out.println("Palidrone");
-
-        }
-        else{
-            System.out.println("NOT palidrone");
-        }
+        System.out.println(isPalindrome("malaoijoalam"));
     }
 }
